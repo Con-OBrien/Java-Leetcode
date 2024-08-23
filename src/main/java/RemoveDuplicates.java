b01d2bc63ae5a79b550eb7ee9b8cee311bfc3423
Conor OBrien <conobr97@gmail.com>
2024-08-23 12:40:37 +0100

    remove dupes & twosum

---
+++ src/main/java/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+public class RemoveDuplicates {
+    public static int removeDuplicates(int[] nums) {
+        int i=0;
+        //Start at pos 1 to end of array
+        //If the latter pos (j) doesn't match the one at 0, we increase i and set the value in the array at i to the one after
+        //i.e. 1,1,2  - nums[j] starts at 1, it does equal the one before, so we dont increase i but we go onto the next iteration
+        //so then we compare 2 to the first value which does not equal it so we enter the if block and increase i and sets the value at nums[i] to the number at nums[j]
+        // that means the value at pos 0, 1, becomes 2, we're at the end of the loop then so i would be 1 so we add 1 we is 2 values
+        for(int j=1; j<nums.length;j++) {
+            if(nums[i]!=nums[j]) {
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2};
+        System.out.println(removeDuplicates(nums));
+    }
+}
---
+++ src/main/java/TwoSum.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] positions = new int[2];
+        boolean found = false;
+
+        for (int i = 0; i< nums.length && !found; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    positions[0] = i;
+                    positions[1] = j;
+                    found = true;
+                    break;
+                }
+            }
+        }
+        return positions;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        int target = 5;
+
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+}
